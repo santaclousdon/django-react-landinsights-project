9e09fe2535704311c1922a674156e27097b2bbf5
santaclousdon <santaclous112@gmail.com>
2024-01-31 16:41:32 -0700

    Swapped feature request form from Google Form to Typeform

--- react_app/src/pages/feature_idea.js
+++ react_app/src/pages/feature_idea.js
@@ -1,11 +1,26 @@
+import React, { useEffect } from 'react';
 import { Card } from "library";
 
-export default function FeatureIdea({}) {
+export default function FeatureIdea() {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "//embed.typeform.com/next/embed.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <Card>
-            <div className="card-header text-center pt-4"></div>
-            <div className="card-body">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfp-J6A80dUmo_5ZuvdiP4Ai0gBO8iC7WOJIXqmwRTSvfLZBw/viewform?embedded=true" width="100%" height="1466" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <div className="card-body text-center">
+            <h6 class="mb-0">Got A Killer Feature Idea?</h6>
+            <p class="text-sm mb-0">Tell us about it now!</p>
+            <hr class="horizontal dark my-3"></hr>
+            <div data-tf-live="01HNGWTNFKF4K7JT4SPGND3AN6"></div>
             </div>
         </Card>
     );
